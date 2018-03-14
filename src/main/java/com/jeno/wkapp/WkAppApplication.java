cfeab020a967aee71f3d7ffea25ae468176f790f
jeno <jeno@genohm.com>
2018-03-14 10:28:05 +0100

    Initial commit

---
+++ src/main/java/com/jeno/wkapp/WkAppApplication.java
@@ -0,0 +1,12 @@
+package com.jeno.wkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jeno/wkapp/WkAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jeno.wkapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WkAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
