3c08c71018917d85caa70c4bdc7e48d417d48c28
jeno <jeno@genohm.com>
2018-05-27 15:56:18 +0200

    Add basic Heroku support

--- src/test/java/com/jeno/fantasyleague/FantasyLeagueApplicationTest.java
+++ src/test/java/com/jeno/fantasyleague/FantasyLeagueApplicationTest.java
@@ -1,26 +1,16 @@
 package com.jeno.fantasyleague;
 
-import com.jeno.fantasyleague.data.service.leaguetemplates.worldcup2018.FifaWorldCup2018Service;
 import org.junit.Test;
-import org.springframework.util.StringUtils;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
-
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class FantasyLeagueApplicationTest {
 
 	@Test
-	public void contextLoads() {
-		Arrays.stream(FifaWorldCup2018Service.Team.values())
-				.forEach(lala ->
-						System.err.println(
-								lala.toString() + "(\"" +
-								StringUtils.capitalize(lala.toString().toLowerCase())
-										.replaceAll("_", " ") +
-								"\", \"images/icons/country_icons/" +
-								lala.toString().toLowerCase() + ".png\"),"));
-		System.err.println("LENGTH " + FifaWorldCup2018Service.Team.values().length);
+	public void test() {
 	}
 
 }
