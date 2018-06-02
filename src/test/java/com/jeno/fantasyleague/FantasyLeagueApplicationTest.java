0d011c37062c2ad613986c40a324a2d5b3fb8573
jeno <jeno@genohm.com>
2018-06-02 18:46:40 +0200

    Comment out unit test

--- src/test/java/com/jeno/fantasyleague/FantasyLeagueApplicationTest.java
+++ src/test/java/com/jeno/fantasyleague/FantasyLeagueApplicationTest.java
@@ -1,16 +1,11 @@
 package com.jeno.fantasyleague;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class FantasyLeagueApplicationTest {
 
-	@Test
-	public void test() {
-	}
+//	@Test
+//	public void test() {
+//	}
 
 }
